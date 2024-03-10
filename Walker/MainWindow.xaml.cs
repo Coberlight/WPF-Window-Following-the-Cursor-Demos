b560eac5c5ce40f882e219fff8ddcce24ed74798
Coberlight <romchik2484@gmail.com>
2024-03-11 02:39:08 +0400

    Added 2 projects with some different animation algorithm
    Added 2 more projects that implements a visual hint when dragging from some UI element of application.
    A default Windows services was used to make such hint. Hint position updates on MouseMove event, so it's not possible to implement a smooth animation of window, because a new window position must be calculated even when user has already stopped moving the mouse cursor. But it is worth considering this method of creating such animations though

---
+++ Walker/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Walker
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow ()
+		{
+			InitializeComponent ();
+		}
+
+		private void mouseMove ( object sender, MouseEventArgs e )
+		{
+			if ( Mouse.Captured == sender )
+			{
+				FrameworkElement elem = sender as FrameworkElement;
+				Point pos = Mouse.GetPosition ( elem );
+				Point screen = elem.PointToScreen ( pos );
+				tb.Text = $"{screen.X}:{screen.Y}";
+				pop.HorizontalOffset = screen.X- (pop.Child as FrameworkElement).ActualWidth / 2;
+				pop.VerticalOffset = screen.Y;
+				if ( !pop.IsOpen )
+					pop.IsOpen = true;
+			}
+		}
+
+		private void mouseDown ( object sender, MouseButtonEventArgs e )
+		{
+			FrameworkElement elem = e.OriginalSource as FrameworkElement;
+			if ( elem != sender )
+			{
+				RenderTargetBitmap rbmp = new RenderTargetBitmap ( ( int ) elem.ActualWidth, ( int ) elem.ActualHeight, 96, 96, PixelFormats.Pbgra32 );
+				rbmp.Render ( elem );
+				content.Source = rbmp;
+				Mouse.Capture ( sender as IInputElement );
+			}
+		}
+
+		private void mouseUp ( object sender, MouseButtonEventArgs e )
+		{
+			if ( Mouse.Captured == sender )
+			{
+				Mouse.Capture ( null );
+				pop.IsOpen = false;
+				tb.Text = "";
+			}
+		}
+	}
+}
